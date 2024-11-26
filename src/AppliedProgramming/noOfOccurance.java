3c6ef043da7cc9b9e655bc01e948212be12f48b6
Zain Badaruddin <zainbadar24@gmail.com>
2024-11-26 23:06:59 +0500

    find second highest occurance

---
+++ src/AppliedProgramming/noOfOccurance.java
@@ -0,0 +1,49 @@
+package AppliedProgramming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class noOfOccurance {
+
+     public  static String str = "madamm";
+
+     public static char findSecondHighestOccurance(){
+
+         Map<Character, Integer> countMap = new HashMap<>();
+         int highestCount = 0;
+         int secondHighestCount=0;
+
+         for (char ch: str.toCharArray()){
+             countMap.put(ch, countMap.getOrDefault(ch,0)+1);
+         }
+
+         for (int count : countMap.values()){
+
+             if(count > highestCount){
+                 secondHighestCount=highestCount;
+                 highestCount=count;
+             }else if(count > secondHighestCount && count < secondHighestCount){
+                 secondHighestCount=count;
+             }
+         }
+
+         for (Map.Entry<Character,Integer> entry : countMap.entrySet()){
+             if(entry.getValue() == secondHighestCount){
+                 return entry.getKey();
+             }
+         }
+
+         return '\0';
+     }
+    public static void main(String[] args) {
+
+      char res =  findSecondHighestOccurance();
+
+      if(res != '\0'){
+          System.out.println(res);
+      }else {
+          System.out.println("there is no second highest");
+      }
+
+    }
+}
